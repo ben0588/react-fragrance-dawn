0f0d9b93a6f00deaf3b906f52823f4df00fc9c82
ben <energy95272z@gmail.com>
2023-08-14 19:27:02 +0800

    優化調整熱門活動區塊

--- src/pages/home/HomeActivity.jsx
+++ src/pages/home/HomeActivity.jsx
@@ -65,37 +65,38 @@ const HomeActivity = () => {
                 <span></span>
             </h2>
 
-            <div className='row'>
-                <div className='col-12 col-lg-6 text-center'>
+            <div className='row align-items-center'>
+                <div className='col-6'>
+                    <strong className='d-flex align-items-center justify-content-center fs-5 pt-3 pb-2'>
+                        <FaStar className='icon-md me-1' />
+                        領取活動優惠碼
+                    </strong>
+                </div>
+                <div className='col-6'>
                     <strong className='d-flex align-items-center justify-content-center fs-5 pt-3 pb-2'>
                         <BiCalendarHeart className='icon-md me-1' />
                         期間限定活動
                     </strong>
+                </div>
+            </div>
+            <div className='row align-items-center'>
+                <div className='col-12 col-lg-6 '>
                     <div className='row border border-3 border-primary'>
                         {sales.map((item, index) => (
                             <motion.div
                                 key={item.id}
-                                className='home-activity-card-container col-6 p-0 m-0'
+                                className=' col-6 p-0 m-0'
                                 initial='hidden'
                                 whileInView='show'
                                 variants={imagesVariants}
                                 custom={index}
                             >
-                                <Link to='/products'>
+                                <Link to='/products' className='home-activity-card-container'>
                                     <h3 className='home-activity-card-title fs-5 fw-bolder p-2'>
                                         {item.title}
                                         <time className='d-block fs-6'>{item.date}</time>
                                         {item.title === activityTarget?.title ? (
-                                            <IoFlameSharp
-                                                style={{
-                                                    position: 'absolute',
-                                                    top: `5px`,
-                                                    left: `5px`,
-                                                    width: `25px`,
-                                                    height: `25px`,
-                                                    fill: `#d63031`,
-                                                }}
-                                            />
+                                            <IoFlameSharp className='home-activity-card-icon' />
                                         ) : null}
                                     </h3>
                                     <img src={item.imageUrl} alt={item.title} className='home-activity-card-image' />
@@ -104,46 +105,49 @@ const HomeActivity = () => {
                         ))}
                     </div>
                 </div>
+
                 <div className='col-12 col-lg-6'>
-                    <strong className='d-flex align-items-center justify-content-center fs-5 pt-3 pb-2'>
-                        <FaStar className='icon-md me-1' />
-                        領取活動優惠碼
-                    </strong>
-                    <div className='row'>
-                        <div className='col-12 d-flex align-items-center justify-content-center flex-column '>
-                            <div className='fs-3 pt-4'>活動舉行中：{activityTarget?.title}</div>
-                            <div className='fs-3 text-ellipsis text-center'>活動舉行期間：{activityTarget?.date}</div>
-                            <div className='d-flex align-items-center justify-content-start fs-4 pt-2 '>
-                                優惠碼：<span className='bg-danger text-white px-2'>50%OFF</span>
-                                <button
-                                    type='button'
-                                    className='btn btn-danger ms-2'
-                                    onClick={() => handleCopyCoupon('50%OFF')}
-                                >
-                                    複製代碼
-                                </button>
+                    <div className='row align-items-stretch'>
+                        <div className='col-12 d-flex align-items-center justify-content-center flex-column h-100 '>
+                            <div className='px-4 py-4'>
+                                <div className='fs-3 text-center'>活動舉行中：{activityTarget?.title}</div>
+                                <div className='fs-3 text-ellipsis text-center'>
+                                    活動舉行期間：{activityTarget?.date}
+                                </div>
+                                <div className='d-flex align-items-center justify-content-center fs-4 '>
+                                    優惠碼：<span className='bg-danger text-white px-2'>50%OFF</span>
+                                    <button
+                                        type='button'
+                                        className='btn btn-danger ms-2'
+                                        onClick={() => handleCopyCoupon('50%OFF')}
+                                    >
+                                        複製代碼
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                        <div className='col-12 d-flex align-items-center justify-content-center flex-column pt-5 px-4 '>
-                            <div className='d-flex align-items-center justify-content-start fs-5 mb-2 '>
-                                <PiWarningCircleBold className='icon-sm pt-1' />
-                                <span className='text-ellipsis'>領取優惠碼時，請注意以下注意事項：</span>
+                        <div className='col-12 d-flex align-items-center justify-content-center flex-column h-100  '>
+                            <div className='px-4 py-4'>
+                                <div className='d-flex align-items-center justify-content-start fs-5 mb-2 '>
+                                    <PiWarningCircleBold className='icon-sm pt-1' />
+                                    <span className='text-ellipsis'>領取優惠碼時，請注意以下注意事項：</span>
+                                </div>
+                                <ol className='text-ellipsis border border-2 ps-4 p-2  '>
+                                    <li>請在使用期限內使用優惠卷，逾期作廢。</li>
+                                    <li>請在使用期限內使用優惠卷，逾期作廢。</li>
+                                    <li>部分優惠卷僅限制特定商品，領取後請注意。</li>
+                                    <li>請在結帳的時候將代碼填寫完畢，並且按下應用。</li>
+                                    <li>優惠卷每個帳號限用一次折扣。</li>
+                                    <li>如有任何疑問，歡迎聯繫客服。</li>
+                                </ol>
+                                <Link
+                                    to='/products'
+                                    role='button'
+                                    className='btn btn-primary btn-primary-hover border border-3  border-primary w-100 py- '
+                                >
+                                    前去逛逛
+                                </Link>
                             </div>
-                            <ol className='text-ellipsis border border-2 ps-4 py-2 mb-4 '>
-                                <li>請在使用期限內使用優惠卷，逾期作廢。</li>
-                                <li>請在使用期限內使用優惠卷，逾期作廢。</li>
-                                <li>部分優惠卷僅限制特定商品，領取後請注意。</li>
-                                <li>請在結帳的時候將代碼填寫完畢，並且按下應用。</li>
-                                <li>優惠卷每個帳號限用一次折扣。</li>
-                                <li>如有任何疑問，歡迎聯繫客服。</li>
-                            </ol>
-                            <Link
-                                to='/products'
-                                role='button'
-                                className='btn btn-primary btn-primary-hover border border-3  border-primary w-75 py-1 mt-3 '
-                            >
-                                前去逛逛
-                            </Link>
                         </div>
                     </div>
                 </div>
