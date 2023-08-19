31286dc6b516f8512d28f0a48d09b949d9c2f012
ben <energy95272z@gmail.com>
2023-08-19 15:22:59 +0800

    Updates

--- assets/AboutBrand-2065483e.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-734983d3.js";const t=()=>s.jsxs("div",{children:[s.jsx("h2",{className:"text-center",children:"品牌理念"}),s.jsx("p",{className:"mt-4",children:"「香氛晨光」的品牌理念是打造一個線上購物中心，提供高品質的香氛產品，讓顧客在家中也能享受到舒適的氛圍。我們致力於挑選最好的香氛產品，並提供專業的建議和服務，讓顧客能夠找到最適合自己的產品。我們希望透過「香氛晨光」，讓每個人都能感受到清新、舒適、自在的生活方式。"})]});export{t as default};
