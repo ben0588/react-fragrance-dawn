253f91898e5bd07b982ab0c24deb203e4ac04ff1
ben <energy95272z@gmail.com>
2023-08-19 16:06:28 +0800

    Updates

--- assets/usePriceToTw-d1d7017e.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-a3c1692d.js";const n=()=>({handlePriceToTw:o.useMemo(()=>r=>{const e=r!==void 0?Math.floor(r):0;return new Intl.NumberFormat("zh-TW",{style:"currency",currency:"TWD",minimumFractionDigits:0}).format(e)},[])});export{n as u};
